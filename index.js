96ec4e8bae53028be1f85493ce8711e212d7bb9c
Brandon Hoffman <brandon.hoffman002@student.swic.edu>
2024-03-22 17:31:51 -0500

    Change code in index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function greet(name) {
-return  name;
+function verifyAdulthood(name, age) {
+  if (age >= 18) {
+    return `Welcome ${name}!`;
+  }
+  return `You are not old enough to enter, ${name}.`;
 }
 
+const name = "Brandon";
+const age = 22;
 
-
-const greeting = greet("Brandon");
-
-
-
-
-console.log("Hello", greeting);
+console.info(verifyAdulthood(name, age));
